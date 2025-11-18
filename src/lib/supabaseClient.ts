4f4c9b1769e331c0ffa7f2b6f440f48959454eed
jamaicarider <championhorse.brand@gmail.com>
2025-11-18 02:08:34 -0300

    foi

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase: SupabaseClient | null =
-  url && key ? createClient(url, key) : null;
+export const supabase: SupabaseClient = createClient(url, key);
